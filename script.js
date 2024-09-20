3ba66c7aba5786d7193415d4659d0cb369240b49
avaneeshdevkota <ad5768@nyu.edu>
2024-09-20 09:16:24 +0400

    Pull paragraphs from Wikipedia

--- script.js
+++ script.js
@@ -1,5 +1,64 @@
+async function getArticleID() {
+
+    let url = "https://en.wikipedia.org/w/api.php?origin=*"; 
+
+    let params = {
+        action: "query",
+        format: "json",
+        list: "random",
+        rnlimit: "1",
+        rnnamespace: "0"
+    };
+
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    try {
+        let response = await fetch(url);
+        let json = await response.json();
+        return json.query.random[0].id;
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+}
+
+async function getArticleContent(articleID) {
+    
+        let url = "https://en.wikipedia.org/w/api.php?origin=*"; 
+    
+        let params = {
+            action: "query",
+            format: "json",
+            prop: "extracts",
+            exintro: "",
+            explaintext: "",
+            pageids: articleID
+        };
+    
+        Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+    
+        try {
+            let response = await fetch(url);
+            let json = await response.json();
+            return json.query.pages[articleID].extract;
+        }
+    
+        catch(error) {
+            console.log(error);
+        }
+}
+
 function preprocessText(text) {
 
+    let word_array = text.split(' ');
+    let n = word_array.length;
+
+    if (n > 60) {
+        word_array = word_array.slice(0, 60);
+    }
+
+    text = word_array.join(' ');
     text = text.toLowerCase();
     text = text.replace(/[^a-z\s]/g, '');
     text = text.replace(/\s+/g, ' ');
@@ -34,6 +93,66 @@ function toInnerHTML(str) {
     return word_array.join('');
 };
 
+async function getRandomArticle() {
+
+    const articleID = await getArticleID();
+    const articleContent = await getArticleContent(articleID);
+    let text = preprocessText(articleContent);
+    let textInner = toInnerHTML(text);
+    return [text, textInner];
+}
+
+async function main() {
+
+    let [text, textInner] = await getRandomArticle();
+
+    let textContainer = document.createElement('div');
+    textContainer.setAttribute('id', 'textContainer')
+    textContainer.innerHTML = textInner;
+    document.body.appendChild(textContainer);
+
+    cursorElement.textContent = '|';
+    updateCursor();
+
+    document.onkeydown = (e) => {
+
+        const pressedKey = e.key;
+
+        if (pressedKey === 'Enter' && currentIndex === text.length) {
+            location.reload();
+        }
+    
+        if (pressedKey === 'Backspace') {
+    
+            previousIndex = currentIndex - 1 < 0 ? 0 : currentIndex - 1
+            document.getElementById(previousIndex).style.color = '#cbcbcb';
+            document.getElementById(previousIndex).style.textDecoration = 'none';
+            currentIndex = previousIndex;
+        }
+    
+        else if (pressedKey === text[currentIndex]) {
+            document.getElementById(currentIndex).style.color = '#818181';
+            currentIndex += 1
+        }
+    
+        else if (pressedKey !== text[currentIndex]) {
+    
+            if (text[currentIndex] === ' ') {
+                document.getElementById(currentIndex).style.textDecoration = 'underline';
+            }
+    
+            document.getElementById(currentIndex).style.color = '#c30101';
+            currentIndex += 1;
+        }
+    
+        if (currentIndex === text.length) {
+            alert('You have successfully typed the text.');
+        }
+    
+        updateCursor();
+    }
+}
+
 function updateCursor() {
 
     cursorElement.remove();
@@ -41,52 +160,7 @@ function updateCursor() {
     parentElement.insertBefore(cursorElement, document.getElementById(currentIndex));
 }
 
-let typeText = "There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. John had planned it exactly this way.";
-typeText = preprocessText(typeText);
-
-let textContainer = document.createElement('div');
-textContainer.setAttribute('id', 'textContainer')
-textContainer.innerHTML = toInnerHTML(typeText);
-document.body.appendChild(textContainer);
-
-
-let currentIndex = 0;
 let cursorElement = document.createElement('span');
 cursorElement.setAttribute('id', 'cursor');
-cursorElement.textContent = '|';
-
-updateCursor();
-
-document.onkeydown = (e) => {
-
-    const pressedKey = e.key;
-
-    if (pressedKey === 'Backspace') {
-
-        previousIndex = currentIndex - 1 < 0 ? 0 : currentIndex - 1
-        document.getElementById(previousIndex).style.color = '#cbcbcb';
-        document.getElementById(previousIndex).style.textDecoration = 'none';
-        currentIndex = previousIndex;
-    }
-
-    else if (pressedKey === typeText[currentIndex]) {
-        document.getElementById(currentIndex).style.color = '#818181';
-        currentIndex += 1
-    }
-
-    else if (pressedKey !== typeText[currentIndex]) {
-
-        if (typeText[currentIndex] === ' ') {
-            document.getElementById(currentIndex).style.textDecoration = 'underline';
-        }
-
-        document.getElementById(currentIndex).style.color = '#c30101';
-        currentIndex += 1;
-    }
-
-    if (currentIndex === typeText.length) {
-        alert('You have successfully typed the text.');
-    }
-
-    updateCursor();
-}
+let currentIndex = 0;
+main();
